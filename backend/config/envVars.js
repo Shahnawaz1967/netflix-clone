906754c5699fc6a7fe8b79296ed8234ba656ac09
Shahnawaz <ashahnawaz010@gmail.com>
2024-09-26 21:07:24 +0530

    modify envvars

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -1,23 +1,13 @@
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 
-// dotenv.config();
-
-// export const ENV_VARS = {
-// 	MONGO_URI: process.env.MONGO_URI,
-// 	PORT: process.env.PORT || 5000,
-// 	JWT_SECRET: process.env.JWT_SECRET,
-// 	NODE_ENV: process.env.NODE_ENV,
-// 	TMDB_API_KEY: process.env.TMDB_API_KEY,
-// };
-import dotenv from 'dotenv';
-
-dotenv.config(); // This loads environment variables from .env file
+dotenv.config();
 
 export const ENV_VARS = {
-  MONGO_URI: process.env.MONGO_URI,
-  PORT: process.env.PORT,
-  NODE_ENV: process.env.NODE_ENV,
-  JWT_SECRET: process.env.JWT_SECRET,
-  TMDB_API_KEY: process.env.TMDB_API_KEY,
+	MONGO_URI: process.env.MONGO_URI,
+	PORT: process.env.PORT || 5000,
+	JWT_SECRET: process.env.JWT_SECRET,
+	NODE_ENV: process.env.NODE_ENV,
+	TMDB_API_KEY: process.env.TMDB_API_KEY,
 };
+
 
